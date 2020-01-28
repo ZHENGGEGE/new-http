40e69468e2c6c2cfde6072fe6d74decc88ceeeba
suzheng <593012736@qq.com>
2020-01-28 17:49:38 +0800

    feat:混合对象和demo

--- examples/server.js
+++ examples/server.js
@@ -27,6 +27,109 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const router = express.Router()
 
+registerSimpleRouter()
+
+registerBaseRouter()
+
+registerErrorRouter()
+
+registerExtendRouter()
+
+function registerSimpleRouter() {
+  router.get('/simple/get', function(req, res) {
+    res.json({
+      msg: `hello world`
+    })
+  })
+}
+
+function registerBaseRouter() {
+  router.get('/base/get', function(req, res) {
+    res.json(req.query)
+  })
+
+  router.post('/base/post', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.post('/base/buffer', function(req, res) {
+    let msg = []
+    req.on('data', chunk => {
+      if (chunk) {
+        msg.push(chunk)
+      }
+    })
+    req.on('end', () => {
+      let buf = Buffer.concat(msg)
+      res.json(buf.toJSON())
+    })
+  })
+}
+
+function registerErrorRouter() {
+  router.get('/error/get', function(req, res) {
+    if (Math.random() > 0.5) {
+      res.json({
+        msg: `hello world`
+      })
+    } else {
+      res.status(500)
+      res.end()
+    }
+  })
+
+  router.get('/error/timeout', function(req, res) {
+    setTimeout(() => {
+      res.json({
+        msg: `hello world`
+      })
+    }, 3000)
+  })
+}
+
+function registerExtendRouter() {
+  router.get('/extend/get', function(req, res) {
+    res.json({
+      msg: 'hello world'
+    })
+  })
+
+  router.options('/extend/options', function(req, res) {
+    res.end()
+  })
+
+  router.delete('/extend/delete', function(req, res) {
+    res.end()
+  })
+
+  router.head('/extend/head', function(req, res) {
+    res.end()
+  })
+
+  router.post('/extend/post', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.put('/extend/put', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.patch('/extend/patch', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.get('/extend/user', function(req, res) {
+    res.json({
+      code: 0,
+      message: 'ok',
+      result: {
+        name: 'jack',
+        age: 18
+      }
+    })
+  })
+}
+
 router.get('/simple/get', function(req, res) {
   res.json({
     msg: `hello world`
--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -8,7 +8,13 @@ export function isDate(val: any): val is Date {
 //   return val !== null && typeof val === 'object'
 // }
 
-export function isPlainObject(val:any):val is Object{
+export function isPlainObject(val: any): val is Object {
   return toString.call(val) === '[object Object]'
 }
 
+export function extend<T, U>(to: T, from: U): T & U {
+  for (const key in from) {
+    ;(to as T & U)[key] = from[key] as any
+  }
+  return to as T & U
+}
