a28467b888a0312b28a0c231ec35c7999f543188
suzheng <593012736@qq.com>
2020-01-30 12:41:09 +0800

    feat:响应数据支持泛型

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -45,28 +45,28 @@ axios('/extend/post', {
   }
 })
 
-// interface ResponseData<T = any> {
-//   code: number
-//   result: T
-//   message: string
-// }
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
 
-// interface User {
-//   name: string
-//   age: number
-// }
+interface User {
+  name: string
+  age: number
+}
 
-// function getUser<T>() {
-//   return axios<ResponseData<T>>('/extend/user')
-//     .then(res => res.data)
-//     .catch(err => console.error(err))
-// }
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
 
-// async function test() {
-//   const user = await getUser<User>()
-//   if (user) {
-//     console.log(user.result.name)
-//   }
-// }
+async function test() {
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.age)
+  }
+}
 
-// test()
+test()
