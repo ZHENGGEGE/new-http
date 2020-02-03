27c0dd4373ed48854d168c50bd236e764d985df4
suzheng <593012736@qq.com>
2020-02-03 19:08:43 +0800

    feat:create方法

--- src/axios.ts
+++ src/axios.ts
@@ -1,15 +1,20 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaults from './defaults'
+import mergeConfig from './core/mergeConfig'
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
   extend(instance, context)
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+
+axios.create = function(config) {
+  return createInstance(mergeConfig(defaults, config))
+}
 
 export default axios
