c6f2614a26616c6b9d866e20b8706a8428e5a365
suzheng <593012736@qq.com>
2020-02-04 19:20:02 +0800

    feat:cancel类的实现

--- src/cancel/CancelToken.ts
+++ src/cancel/CancelToken.ts
@@ -1,17 +1,18 @@
 import { CancelExecutor, Canceler, CancelTokenSource } from '../types'
+import Cancel from './Cancel'
 
 interface ResolvePromise {
-  (reason?: string): void
+  (reason?: Cancel): void
 }
 
 export default class CancelToken {
-  promise: Promise<string>
-  reason?: string
+  promise: Promise<Cancel>
+  reason?: Cancel
 
   constructor(executor: CancelExecutor) {
     let resolvePromise: ResolvePromise
 
-    this.promise = new Promise<string>(resolve => {
+    this.promise = new Promise<Cancel>(resolve => {
       resolvePromise = resolve
     })
 
@@ -19,9 +20,15 @@ export default class CancelToken {
       if (this.reason) {
         return
       }
-      this.reason = message
+      this.reason = new Cancel(message)
       resolvePromise(this.reason)
     })
+  }
+
+  throwIfRequested() {
+    if (this.reason) {
+      throw this.reason
+    }
   }
 
   static source(): CancelTokenSource {
--- src/types/index.ts
+++ src/types/index.ts
@@ -83,6 +83,9 @@ export interface AxiosInstance extends Axios {
 
 export interface AxiosStatic extends AxiosInstance {
   create(config?: AxiosRequestConfig): AxiosInstance
+  CancelToken: CancelTokenStatic
+  Cancel: CancelStatic
+  isCancel: (value: any) => boolean
 }
 
 export interface AxiosInterceptorManager<T> {
@@ -104,8 +107,10 @@ export interface AxiosTransformer {
 }
 
 export interface CancelToken {
-  promise: Promise<string>
-  reason?: string
+  promise: Promise<Cancel>
+  reason?: Cancel
+
+  throwIfRequested(): void
 }
 
 export interface Canceler {
@@ -125,4 +130,12 @@ export interface CancelTokenStatic {
   new (executer: CancelExecutor): CancelToken
 
   source(): CancelTokenSource
+}
+
+export interface Cancel {
+  message?: string
+}
+
+export interface CancelStatic {
+  new (message?: string): Cancel
 }
